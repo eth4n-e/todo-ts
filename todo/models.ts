6b38d889dde6aa7906dd03b6d5781b88c05dea60
eth4n-e <etep9497@colorado.edu>
2025-05-30 08:55:39 -0600

    struggling to get tests to working without errors currently

--- todo/models.ts
+++ todo/models.ts
@@ -1,8 +1,13 @@
-export enum Priority {
-  Low,
-  Medium,
-  High,
-}
+const Priority = {
+  LOW: 'low',
+  MEDIUM: 'medium',
+  HIGH: 'high'
+} as const; // as const treats object as immutable and literal
+
+// keyof typeof status returns keys of object (LOW, ...)
+// (typeof Priority)[keyof typeof Priority] indexes the object 
+// result: type Priority = 'low' | 'medium' | 'high';
+type Priority = (typeof Priority)[keyof typeof Priority];
 
 export type Task = {
   id: number;
