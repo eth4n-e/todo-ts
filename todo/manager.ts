66f7f177158d0bee2b68c9ea7ad613487945fe34
eth4n-e <etep9497@colorado.edu>
2025-05-30 20:33:40 -0600

    adding everything

--- todo/manager.ts
+++ todo/manager.ts
@@ -18,17 +18,48 @@ export function addTask(desc: string, priority: Priority, duration: number) {
 }
 
 export function removeTask(id: number) {
+  const tasks: Task[] = loadTasks();
+  // remove task from list
+  const filteredTasks: Task[] = tasks.filter((task) => task.id != id);
+  saveTasks(filteredTasks);
 }
 
 export function listTasks(): Task[] {
   return loadTasks();
 }
 
-export function updateStatus(id: number, status: boolean) {
+export function updateStatus(id: number) {
+  const tasks: Task[] = loadTasks();
+  // update tasks in place
+  tasks.forEach((task) => {
+    if (task.id == id) {
+      task.done = !task.done;
+    }
+  });
+
+  saveTasks(tasks);
 }
 
 export function updatePriority(id: number, priority: Priority) {
+  const tasks: Task[] = loadTasks();
+  // update tasks in place
+  tasks.forEach((task) => {
+    if (task.id == id) {
+      task.priority = priority;
+    }
+  });
+
+  saveTasks(tasks);
 }
 
 export function updateDuration(id: number, duration: number) {
+  const tasks: Task[] = loadTasks();
+  // update tasks in place
+  tasks.forEach((task) => {
+    if (task.id == id) {
+      task.duration = duration;
+    }
+  });
+
+  saveTasks(tasks);
 }
