af60fc7f929abb11a357e6d6f0c76d3ed81bcf3b
eth4n-e <etep9497@colorado.edu>
2025-07-02 09:15:59 -0600

    created initial prompt with add / remove task options

--- src/todo/models.ts
+++ src/todo/models.ts
@@ -4,10 +4,21 @@ export const Priority = {
   HIGH: "high",
 } as const; // as const treats object as immutable and literal
 
-// keyof typeof status returns keys of object (LOW, ...)
+// keyof typeof returns keys of object (LOW, ...)
 // (typeof Priority)[keyof typeof Priority] indexes the object
 // result: type Priority = 'low' | 'medium' | 'high';
 export type Priority = (typeof Priority)[keyof typeof Priority];
+
+export const Choice = {
+  ADD: "ADD",
+  REMOVE: "REMOVE",
+  MODIFY: "MODIFY",
+  LIST: "LIST",
+  SORT: "SORT",
+  QUIT: "QUIT",
+} as const;
+
+export type Choice = (typeof Choice)[keyof typeof Choice];
 
 export type TaskData = {
   desc: string;
